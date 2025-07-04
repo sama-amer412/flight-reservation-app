8641b1c1bed4a9966a3f493df575232c89e6881b
sama-amer412 <samaahmed01009561195@gmail.com>
2025-07-05 02:50:46 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,20 @@
 import sqlite3
+import os
+import sys
 
-def book_flight(name,flight_number,departure,destination,date,seat_number):
-    con=sqlite3.connect("flights.db")
+
+# ⬇ Get correct path for .exe and .py
+def get_db_path():
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, "flights.db")
+
+
+def book_flight(name,flight_number,departure,destination,date,seat_number,time):
+
+    db_path = get_db_path()
+    con = sqlite3.connect(db_path)
     cur=con.cursor()
+    # cur.execute("DROP TABLE flights")
     cur.execute("""CREATE TABLE IF NOT EXISTS flights (
     reservation_id INTEGER PRIMARY KEY AUTOINCREMENT,
     Name TEXT,
@@ -10,15 +22,17 @@ def book_flight(name,flight_number,departure,destination,date,seat_number):
     Departure TEXT,
     Destination TEXT,
     Date TEXT,
-    Seat_Number TEXT)""")
-    cur.execute("""INSERT INTO flights (Name,Flight_Number,Departure,Destination,Date,Seat_Number) 
-    VALUES(?,?,?,?,?,?)""",(name,flight_number,departure,destination,date,seat_number))
+    Seat_Number TEXT,
+    Time TEXT)""")
+    cur.execute("""INSERT INTO flights (Name,Flight_Number,Departure,Destination,Date,Seat_Number,Time) 
+     VALUES(?,?,?,?,?,?,?)""",(name,flight_number,departure,destination,date,seat_number,time))
 
 
     con.commit()
     con.close()
 def view_reservation():
-    con = sqlite3.connect("flights.db")
+    db_path = get_db_path()
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
     stored_data=cur.execute("SELECT * FROM flights ")
     data=[]
@@ -29,7 +43,9 @@ def view_reservation():
     con.close()
     return data
 def delete_reservation(reservation_id):
-    con = sqlite3.connect("flights.db")
+    # con = sqlite3.connect("flights.db")
+    db_path = get_db_path()
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
     cur.execute("DELETE FROM flights WHERE reservation_id=? ",(reservation_id,))
     con.commit()
@@ -37,7 +53,9 @@ def delete_reservation(reservation_id):
     return True
 def edit_reservation(reservation_id,updated_lst):
 
-     con=sqlite3.connect("flights.db")
+     # con=sqlite3.connect("flights.db")
+     db_path = get_db_path()
+     con = sqlite3.connect(db_path)
      cur=con.cursor()
      cur.execute("""UPDATE flights SET
       Name=? ,
@@ -45,7 +63,8 @@ def edit_reservation(reservation_id,updated_lst):
       Departure=?,
       Destination=?,
       Date=?,
-      Seat_Number=?
+      Seat_Number=?,
+      Time=?
      WHERE reservation_id=?""",(*updated_lst,reservation_id))
      con.commit()
      con.close()
